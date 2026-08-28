2c97138b60d66590e327562a3963d2f527120a65
551UK <wdf@hotmail.co.uk>
2026-08-28 21:41:37 +0100

    Delete EveryNotifyVibePrefs directory

--- EveryNotifyVibePrefs/ENVApplicationListController.h
+++
@@ -1,7 +0,0 @@
-#import <Preferences/PSListController.h>
-
-@interface ENVApplicationListController : PSListController {
-    NSMutableSet<NSString *> *_disabledApps;
-    NSArray *_applications;
-}
-@end
